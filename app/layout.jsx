7f3404e602d6cc6ff3d1ec69d525cebb08e92a83
abhint1 <abhint2603@gmail.com>
2024-03-12 23:05:04 +0530

    Remove unused files and update configuration

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "prompter",
+  description: "A simple prompter for your presentations",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
