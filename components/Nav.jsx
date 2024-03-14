e5e8f2bf0a3ac4221b8bc26748d9baafe3a3bc2f
abhint1 <abhint2603@gmail.com>
2024-03-14 23:41:23 +0530

    Update Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,9 +5,20 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { set } from "mongoose";
 
 function Nav() {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+  const [providers, setproviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+    
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -19,8 +30,32 @@ function Nav() {
           height={30}
           className="object-contain"
         />
-        <p className="logi_text">Prompter</p>
+        <p className="logo_text">Prompter</p>
       </Link>
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="assests/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 }
